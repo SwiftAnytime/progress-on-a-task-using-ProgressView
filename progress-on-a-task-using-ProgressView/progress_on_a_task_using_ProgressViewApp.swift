19e86bdf8dbb23b60ac04561c74c10a5827ef54a
Etishagarg <gargetisha@gmail.com>
2024-07-30 00:04:03 +0530

    Initial Commit

---
+++ progress-on-a-task-using-ProgressView/progress_on_a_task_using_ProgressViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  progress_on_a_task_using_ProgressViewApp.swift
+//  progress-on-a-task-using-ProgressView
+//
+//  Created by Etisha Garg on 30/07/24.
+//
+
+import SwiftUI
+
+@main
+struct progress_on_a_task_using_ProgressViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
